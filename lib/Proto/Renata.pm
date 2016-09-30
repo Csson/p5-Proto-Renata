def7f17b20d1e93af621ccfae3406c82bdf94849
Erik Carlsson <info@code301.com>
2016-09-30 23:14:01 +0000

    Init

---
+++ lib/Proto/Renata.pm
@@ -0,0 +1,29 @@
+use 5.10.0;
+use strict;
+use warnings;
+
+package Proto::Renata;
+
+# ABSTRACT: Short intro
+# AUTHORITY
+our $VERSION = '0.0100';
+
+
+
+1;
+
+__END__
+
+=pod
+
+=head1 SYNOPSIS
+
+    use Proto::Renata;
+
+=head1 DESCRIPTION
+
+Proto::Renata is ...
+
+=head1 SEE ALSO
+
+=cut
---
+++ t/01-basic.t
@@ -0,0 +1,9 @@
+use strict;
+use warnings;
+use Test::More;
+use if $ENV{'AUTHOR_TESTING'}, 'Test::Warnings';
+
+use Proto::Renata;
+ok 1, 'Loaded';
+
+done_testing;
